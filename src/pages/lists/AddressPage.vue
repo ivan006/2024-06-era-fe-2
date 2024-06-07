174a1362c8a04eb7fbf5fab756082de8bfb5f242
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-06-07 23:18:47 +0200

    1

---
+++ src/pages/lists/AddressPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <SuperTable :model="address" />"
+  </div>
+</template>
+
+<script>
+import {SuperTable} from "quicklists-vue-orm-ui-2";
+import Address from "src/models/Address";
+export default {
+  name: "AddressPage2",
+  components: {SuperTable},
+  data(){
+    return {
+      address: Address
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore } from 'vuex'
 import VuexORM from '@vuex-orm/core'
 import User from 'src/models/User'
+import Address from "src/models/Address";
+import {DBCrudCacheSet} from "quicklists-vue-orm-ui-2";
 
 // Initialize the database
 const database = new VuexORM.Database()
 
 // Register models
 database.register(User)
+database.register(Address)
+database.register(DBCrudCacheSet)
 
 // Create Vuex store
 const store = createStore({
